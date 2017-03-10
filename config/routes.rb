a863c881537deeaea1979cad46165adc42e954b8
richjdsmith <rich@richjdsmith.ca>
2017-03-09 17:41:23 -0800

    Add coments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,3 +14,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
+
+
+#get "/posts" => "posts#index"
+#get "/posts/:id" => "posts#show"
+#get "/posts/new" => "posts#new"
+#post "/posts" => "posts#create"  # usually a submitted form
+#get "/posts/:id/edit" => "posts#edit"
+#put "/posts/:id" => "posts#update" # usually a submitted form
+#delete "/posts/:id" => "posts#destroy"
