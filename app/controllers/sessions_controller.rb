a7098f6f55d4a92a2eff050b9bf27f7ab4eb2087
richjdsmith <rich@richjdsmith.ca>
2017-03-13 18:48:01 -0700

    Impliment advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user
       # Log the user in and redirect to the user's show page.
     else
@@ -14,8 +19,11 @@ class SessionsController < ApplicationController
     end
   end
 
+
+
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
